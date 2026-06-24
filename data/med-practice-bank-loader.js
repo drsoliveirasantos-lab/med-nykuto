e4a6c010b8f20a046213735f4c3890d5d31f533a
drsoliveirasantos-lab <drs.oliveirasantos@gmail.com>
2026-06-24 23:51:48 +0200

    Load Microbiología M1 QCM 061-080 patch v316

--- data/med-practice-bank-loader.js
+++ data/med-practice-bank-loader.js
@@ -9,7 +9,7 @@
   var bankFiles = {"bioquimica":"practice-bank-bioquimica.js","fisiologia":"practice-bank-fisiologia.js","genetica":"practice-bank-genetica.js","inmunologia":"practice-bank-inmunologia.js","microbiologia":"practice-bank-microbiologia.js"};
   var patchFiles = {
     "fisiologia":["practice-bank-fisiologia-quality-patch-v314.js","practice-bank-fisiologia-quality-patch-v315.js"],
-    "microbiologia":["practice-bank-microbiologia-quality-patch-v312.js","practice-bank-microbiologia-quality-patch-v313.js","practice-bank-microbiologia-quality-patch-v314.js","practice-bank-microbiologia-quality-patch-v315.js"]
+    "microbiologia":["practice-bank-microbiologia-quality-patch-v312.js","practice-bank-microbiologia-quality-patch-v313.js","practice-bank-microbiologia-quality-patch-v314.js","practice-bank-microbiologia-quality-patch-v315.js","practice-bank-microbiologia-quality-patch-v316.js"]
   };
 
   function normId(x){
