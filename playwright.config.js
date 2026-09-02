816cf8fc6f53a3b16db46aeac67892a5def8571e
drsoliveirasantos-lab <drs.oliveirasantos@gmail.com>
2026-09-02 04:17:54 +0200

    test: allow exhaustive 23-lesson browser audits to finish

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
-  timeout: 90_000,
+  timeout: 150_000,
   expect: { timeout: 15_000 },
   fullyParallel: false,
   workers: 1,
